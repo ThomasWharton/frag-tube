a3239a42a565b1a1ef373d70664d91c571c8f8cc
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-19 20:06:44 +0000

    Install django, create project and link cloudinary

---
+++ frag_tube/frag_tube/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for frag_tube project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frag_tube.settings')
+
+application = get_asgi_application()
