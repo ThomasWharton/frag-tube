9c01c2d8105b43e2aac117e3d63a53a1846a73df
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 06:52:47 +0000

    Feat: Update ProfileDetail queryset

--- profiles/views.py
+++ profiles/views.py
@@ -24,5 +24,9 @@ class ProfileList(generics.ListAPIView):
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count = Count('owner__post', distinct=True),
+        followers_count = Count('owner__followed', distinct=True),
+        following_count = Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
