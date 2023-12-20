fc78bd90c25787d2ffd78124e6ebf5b86bd0e523
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 04:07:16 +0000

    Feat: Add comments serializers

---
+++ comments/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.humanize import naturaltime
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'post', 'created_at',
+            'updated_at', 'content'
+        ]
+
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
