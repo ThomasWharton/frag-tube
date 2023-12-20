ae05bcddc5c6b4bacfe2c3c8bcf08cd739a7d1bb
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 04:54:08 +0000

    Feat: Add likes serializers

---
+++ likes/serializers.py
@@ -0,0 +1,22 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'owner',
+            'post', 'created_at'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializer.ValidationError({
+                'detail': 'possible duplicate'
+            })
