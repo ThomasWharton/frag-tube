76889aacef636dc9275e80c0007abd1613c1196a
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 03:56:53 +0000

    Feat: Add comment model

---
+++ comments/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.content
