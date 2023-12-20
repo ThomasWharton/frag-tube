61d0ba04be8dead2035f280f5bcc0f140ce337a7
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 04:58:08 +0000

    Feat: Add likes views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from frag_tube.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Likes.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+
+class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Likes.objects.all()
