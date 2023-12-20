47634e683ab7198e1adc0d99ff3ed5a890bab05a
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 05:37:30 +0000

    Fix: Missing slash in likes url

--- likes/urls.py
+++ likes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from likes import views
 
 urlpatterns = [
-    path('likes', views.LikeList.as_view()),
+    path('likes/', views.LikeList.as_view()),
     path('likes/<int:pk>', views.LikeDetail.as_view()),
 ]
