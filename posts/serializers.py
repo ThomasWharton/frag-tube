d6d7cbf6be8a7bb4242c1e3fe42ac4b972682fb4
ThomasWharton <thomaswharton7@hotmail.com>
2023-12-20 03:45:39 +0000

    Feat: Add postdetail view with create, edit and delete functionality

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_is_owner(self, obj):
         request = self.context['request']
-        return request.iser == obj.owner
+        return request.user == obj.owner
 
     class Meta:
         model = Post
