a185f6c3c14ee97373f132d64399af3b5b5ac21f
ThomasWharton <thomaswharton7@hotmail.com>
2024-01-19 20:33:20 +0000

    Fix: Add missing comma to line 95 settings

--- frag_tube/settings.py
+++ frag_tube/settings.py
@@ -92,7 +92,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
-    'corsheaders'
+    'corsheaders',
     'profiles',
     'posts',
     'comments',
