3a2c6974ef63759d9ce8f14bb8fcb839bd058243
ThomasWharton <thomaswharton7@hotmail.com>
2024-01-21 22:18:18 +0000

    Feat: Link up routes to navbar links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home page</h1>
-        <h1>Sign in</h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+        </Switch>
       </Container>
     </div>
   );
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from '../styles/NavBar.module.css'
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand className={styles.Logo}>
-          Frag<i className="fas fa-rocket"></i>Tube
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.Logo}>
+            Frag<i className="fas fa-rocket"></i>Tube
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link className={styles.Link}>
+            <NavLink to="/" className={styles.Link}>
               <i className="fas fa-home"></i> Home
-            </Nav.Link>
-            <Nav.Link className={styles.Link}>
+            </NavLink>
+            <NavLink to="/signin" className={styles.Link}>
               <i className="fas fa-sign-in-alt"></i> Sign in
-            </Nav.Link>
-            <Nav.Link className={styles.Link}>
+            </NavLink>
+            <NavLink to="/signup" className={styles.Link}>
               <i className="fas fa-user-plus"></i> Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
