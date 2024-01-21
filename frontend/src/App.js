7ecd783b79f349bf507aae8827824ae7a923e3ea
ThomasWharton <thomaswharton7@hotmail.com>
2024-01-21 22:04:47 +0000

    Feat: Add main style class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home page</h1>
+        <h1>Sign in</h1>
+      </Container>
     </div>
   );
 }
