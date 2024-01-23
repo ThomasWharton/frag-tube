f55282c23d270fc513d8b9ef8bb203b6c6c6fd2f
ThomasWharton <thomaswharton7@hotmail.com>
2024-01-23 17:36:00 +0000

    Feat: Install axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
+import './api/axiosDefaults';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={()=><p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
+
