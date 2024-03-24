aa85d51796ed13484002a75ee3ef5bff5196ed0d
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-24 20:45:15 +0000

    Feat: Add serializer for rating model

---
+++ ratings/serializers.py
@@ -0,0 +1,21 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Rating
+
+class RatingSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Rating
+        fields = [
+            'id', 'owner',
+            'post', 'created_at'
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializer.ValidationError({
+                'detail': 'possible duplicate'
+            })
