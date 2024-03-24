bc7042f89a9a5dbe5983eef3aabff8f116b00d4c
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-24 20:39:43 +0000

    Feat: Update post serializer to include rating details

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 from likes.models import Like
+from ratings.models import Rating
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -11,6 +12,8 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    rating_id = serializers.SerializerMethodField()
+    ratings_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -24,6 +27,15 @@ class PostSerializer(serializers.ModelSerializer):
             ).first()
             return like.id if like else None
         return None
+    
+    def get_rating_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            rating = Rating.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return rating.id if rating else None
+        return None
 
     class Meta:
         model = Post
@@ -31,5 +43,6 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'category', 'title', 'content', 'embed_id',
-            'like_id', 'likes_count', 'comments_count'
+            'like_id', 'likes_count', 'comments_count', 
+            'rating_id', 'ratings_count'
         ]
