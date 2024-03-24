4ca3884e6e0fde08e607444ea4e47918c70bf49b
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-24 20:30:13 +0000

    Feat: Add rating app and build model

---
+++ ratings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+class Rating(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
+    rating = models.PositiveIntegerField(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+    
+    def __str__(self):
+        return f"{self.owner}'s {self.rating} for {self.post}"
