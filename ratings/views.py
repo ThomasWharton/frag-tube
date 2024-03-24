2042731fb415e5ce65cf450516dd7b270ca25697
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-24 20:49:31 +0000

    Feat: Add view for ratings app

--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from frag_tube.permissions import IsOwnerOrReadOnly
+from .models import Rating
+from .serializers import RatingSerializer
 
-# Create your views here.
+
+class RatingList(generics.ListCreateAPIView):
+    serializer_class = RatingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Rating.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
+
+class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
