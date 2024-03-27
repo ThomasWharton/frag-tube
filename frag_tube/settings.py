fa7b1075e11371bddd6fde4f088061bab7acf635
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-27 16:51:46 +0000

    Fix: Issue allowing user to submit rating

--- frag_tube/settings.py
+++ frag_tube/settings.py
@@ -156,18 +156,11 @@ WSGI_APPLICATION = 'frag_tube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-    print('connected')
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
 
 
 # Password validation
--- ratings/serializers.py
+++ ratings/serializers.py
@@ -15,13 +15,14 @@ class RatingSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = self.context['request'].user
         post = validated_data['post']
+        rating_value = validated_data.get('rating')
         
         try:
             # Check if a rating instance exists for the current user and post
             instance = Rating.objects.get(owner=user, post=post)
-            instance.rating = validated_data.get('rating', instance.rating)
+            instance.rating = rating_value if rating_value is not None else instance.rating
             instance.save()
             return instance
         except Rating.DoesNotExist:
             # Create a new rating instance
-            return Rating.objects.create(owner=user, **validated_data)
+            return Rating.objects.create(**validated_data)
