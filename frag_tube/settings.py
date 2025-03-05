f3c14628beec68a7c5d2bc8cf2f1f1201edf08d5
Thomas <thomaswharton7@hotmail.com>
2025-03-05 16:08:25 +0000

    Remove old cors code

--- frag_tube/settings.py
+++ frag_tube/settings.py
@@ -120,15 +120,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN')
-#     ]
-# if 'CLIENT_ORIGIN_DEV' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN_DEV')
-#     ]
-
 CORS_ALLOWED_ORIGINS = [os.environ.get('CLIENT_ORIGIN'), os.environ.get('CLIENT_ORIGIN_DEV')]
 
 CORS_ALLOW_CREDENTIALS = True
